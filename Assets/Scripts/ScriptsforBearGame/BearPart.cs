514b2745b35f76dfdb0cd0ff0ae99dbfefd35c87
pincurow <pincurow@OA-S213-10>
2026-02-12 21:10:51 -0500

    Realized I was doing the wrong thing. Started back up adding a slider and trying to rig it to move the bear part.

--- Assets/Scripts/ScriptsforBearGame/BearPart.cs
+++ Assets/Scripts/ScriptsforBearGame/BearPart.cs
@@ -3,10 +3,7 @@ using UnityEngine.UIElements;
 
 public class BearPart : MonoBehaviour
 {
-    public bool movingUp;
-    public bool movingLeft;
-    public bool movingDown;
-    public bool movingRight;
+    public float transformValue;
 
     void Start()
     {
@@ -16,55 +13,13 @@ public class BearPart : MonoBehaviour
     void Update()
     {
         Vector3 position = transform.position;
-        if(movingUp)
-        {
-            position.y += 0.1f;
-        }
-        if (movingRight)
-        {
-            position.x += 0.1f;
-        }
-        if (movingDown)
-        {
-            position.y -= 0.1f;
-        }
-        if (movingLeft)
-        {
-            position.x -= 0.1f;
-        }
-
+        position.y = transformValue;
         transform.position = position;
     }
 
-    public void buttonWasPressed()
+    public void setTransform(float temp)
     {
-
-    }
-
-    void moveTeddy(int direction)
-    {
-        Vector3 position = transform.position;
-        if(direction == 1)
-        {
-            movingUp = true;
-        }
-        if (direction == 2)
-        {
-            movingRight = true;
-        }
-        if (direction == 3)
-        {
-            movingDown = true;
-        }
-        if (direction == 4)
-        {
-            movingLeft = true;
-        }
-    }
-
-    public void makeBearList()
-    {
-
+        transformValue = temp;
     }
 }
 
