74e7aafbaa2d775a4e3d7c5443b146fc2e81ba9b
pincurow <pincurow@OA-S141-18>
2026-02-18 16:23:14 -0500

    Continued the project. Adding setsize and color to the bear parts. Bearpart prefab almost complete.

--- Assets/Scripts/ScriptsforBearGame/BearPart.cs
+++ Assets/Scripts/ScriptsforBearGame/BearPart.cs
@@ -1,8 +1,15 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class BearPart : MonoBehaviour
 {
+    SpriteRenderer mySpriteRenderer;
+    public float smallSize;
+    public float largeSize;
+    public Color darkColour;
+    public Color lightColour;
     public float transformValue;
 
     void Start()
@@ -20,6 +27,30 @@ public class BearPart : MonoBehaviour
     public void setTransform(float temp)
     {
         transformValue = temp;
+    }
+
+    public void SetColor(bool colorChoice)
+    {
+        if(colorChoice == true)
+        {
+            mySpriteRenderer.color = darkColour;
+        }
+        else
+        {
+            mySpriteRenderer.color = lightColour;
+        }
+    }
+
+    public void SetSize(bool bigOrSmall)
+    {
+        if(bigOrSmall == true)
+        {
+            transform.localScale = new Vector3(smallSize, smallSize, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(largeSize, largeSize, 1);
+        }
     }
 }
 
---
+++ Assets/Scripts/ScriptsforBearGame/ScriptToSpawn.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptToSpawn : MonoBehaviour
+{
+   public GameObject spawnBearPart;
+   float bearPartsSpawned;
+
+   private List<GameObject> spawnedParts = new List<GameObject>();
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void bearPartList()
+    {
+        
+    }
+
+    public void spawnBearPartMethod()
+    {
+        Instantiate(spawnBearPart, transform.position, Quaternion.identity);
+    }
+}
