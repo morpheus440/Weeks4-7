b9cb29a9b09cafcf04bc5ea3bcd5efaf18679916
pincurow <pincurow@OA-S213-10>
2026-03-12 21:44:07 -0400

    resaving in case i forgot to save scene

--- Assets/Scripts/cactusScript.cs
+++ Assets/Scripts/cactusScript.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-public class ContactSensor : MonoBehaviour
+public class cactusScript : MonoBehaviour
 {
     public GameObject player;
     public EventDrivenLara playerThing;
