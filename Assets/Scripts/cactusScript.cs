6d8a2f02f04df8081a6f44b66eb6612b86aef03f
pincurow <pincurow@OA-S213-10>
2026-03-12 21:40:32 -0400

    Tried making it so the player is damaged when entering cactus

---
+++ Assets/Scripts/cactusScript.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ContactSensor : MonoBehaviour
+{
+    public GameObject player;
+    public EventDrivenLara playerThing;
+    public UnityEvent OnEnter;
+    public UnityEvent OnLeave;
+
+    private SpriteRenderer hazardRenderer;
+    private bool wasInTheHazard = false;
+
+
+    void Start()
+    {
+        hazardRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        bool isInTheHazard = hazardRenderer.bounds.Contains(player.transform.position);
+        if (isInTheHazard
+            && wasInTheHazard == false)
+        {
+            wasInTheHazard = true;
+            Debug.Log("Player entered the hazard");
+        }
+        else if (!isInTheHazard && wasInTheHazard)
+        {
+            wasInTheHazard = false;
+            Debug.Log("Player left the hazard");
+        }
+        if (isInTheHazard)
+        {
+            playerThing = GetComponent<EventDrivenLara>();
+        }
+        else
+        {
+
+        }
+    }
+}
